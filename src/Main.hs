d54148809dc39c739ff0fd447bc920bc1166424c
wanglijun <juns1984@qq.com>
2016-08-04 16:50:57 +0800

    add

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,26 @@ module Main where
 import Database.HDBC
 import Database.HDBC.PostgreSQL
 
+data Entity = Department Int | SubCompany Int | Member Int deriving(Show,Eq,Read,Ord)
+
+t2i :: Entity -> Int
+t2i (Member _)     = 0
+t2i (Department _) = 1
+t2i (SubCompany _) = 2
+
+eid :: Entity -> String
+eid (Member id')     = 'm':show id'
+eid (Department id') = 'd':show id'
+eid (SubCompany id') = 's':show id'
+
+value :: Entity -> [SqlValue]
+value e@(Member id') = [toSql . eid $ e, toSql id']
+value e = [toSql . eid $ e, toSql . t2i $ e]
+
+istmt (Member _)     = "INSERT INTO member VALUES (?, ?)"
+istmt (Department _) = "INSERT INTO department VALUES (?, 1)"
+istmt (SubCompany _) = "INSERT INTO department VALUES (?, 2)"
+
 main = do
    cn <- conn
    dset <- quickQuery' cn "select * from path" []
@@ -10,4 +30,19 @@ main = do
    disconnect cn
 
 conn = connectPostgreSQL "host=192.168.99.100 dbname=hello user=postgres"
+
+newe :: Connection -> [Entity] -> IO [Integer]
+newe conn [] = return []
+newe conn es@(e:_) = do
+    stmt <- prepare conn (istmt e)
+    rs <- mapM (execute stmt) (map value es)
+    commit conn
+    return rs
+
+neweIO :: [Entity] -> IO [Integer]
+neweIO es = do
+   cn <- conn
+   rs <- newe cn es
+   disconnect cn
+   return rs
 
