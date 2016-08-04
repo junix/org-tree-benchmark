cf5a162c59ef1019a20be7a15f0cc5ee61baf184
wanglijun <juns1984@qq.com>
2016-08-04 09:49:39 +0800

    add

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,13 @@
 module Main where
 
+import Database.HDBC
+import Database.HDBC.PostgreSQL
+
 main = do
-    putStrLn "hello"
+   cn <- conn
+   dset <- quickQuery' cn "select * from path" []
+   print dset
+   disconnect cn
+
+conn = connectPostgreSQL "host=192.168.99.100 dbname=hello user=postgres"
 
