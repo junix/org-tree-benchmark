e534bb342b6d64e717351646a1144108065e782e
wanglijun <juns1984@qq.com>
2016-08-10 14:12:58 +0800

    并行创建表

--- src/NiceFork.hs
+++ src/NiceFork.hs
@@ -1,44 +1,17 @@
--- file: ch24/NiceFork.hs
-module NiceFork
-    (
-      ThreadManager
-    , newManager
-    , forkManaged
-    , getStatus
-    , waitFor
-    , waitAll
-    ) where
-
--- file: ch24/NiceFork.hs
+module NiceFork where
 import Control.Concurrent
 import Control.Monad (join)
 import qualified Data.Map as M
 
 data ThreadStatus = Running | Finished deriving (Eq, Show)
+newtype ThreadManager = Mgr (MVar (M.Map ThreadId (MVar ThreadStatus))) deriving (Eq)
 
 -- | Create a new thread manager.
 newManager :: IO ThreadManager
+newManager = Mgr `fmap` newMVar M.empty
 
 -- | Create a new managed thread.
 forkManaged :: ThreadManager -> IO () -> IO ThreadId
-
--- | Immediately return the status of a managed thread.
-getStatus :: ThreadManager -> ThreadId -> IO (Maybe ThreadStatus)
-
--- | Block until a specific managed thread terminates.
-waitFor :: ThreadManager -> ThreadId -> IO (Maybe ThreadStatus)
-
--- | Block until all managed threads terminate.
-waitAll :: ThreadManager -> IO ()
-
--- file: ch24/NiceFork.hs
-newtype ThreadManager =
-    Mgr (MVar (M.Map ThreadId (MVar ThreadStatus)))
-    deriving (Eq)
-
-newManager = Mgr `fmap` newMVar M.empty
-
--- file: ch24/NiceFork.hs
 forkManaged (Mgr mgr) body =
     modifyMVar mgr $ \m -> do
       state <- newEmptyMVar
@@ -47,7 +20,8 @@ forkManaged (Mgr mgr) body =
         putMVar state Finished
       return (M.insert tid state m, tid)
 
--- file: ch24/NiceFork.hs
+-- | Immediately return the status of a managed thread.
+getStatus :: ThreadManager -> ThreadId -> IO (Maybe ThreadStatus)
 getStatus (Mgr mgr) tid =
   modifyMVar mgr $ \m ->
     case M.lookup tid m of
@@ -56,7 +30,8 @@ getStatus (Mgr mgr) tid =
                    Nothing -> return (m, Just Running)
                    Just sth -> return (M.delete tid m, Just sth)
 
--- file: ch24/NiceFork.hs
+-- | Block until a specific managed thread terminates.
+waitFor :: ThreadManager -> ThreadId -> IO (Maybe ThreadStatus)
 waitFor (Mgr mgr) tid = do
   maybeDone <- modifyMVar mgr $ \m ->
     return $ case M.updateLookupWithKey (\_ _ -> Nothing) tid m of
@@ -66,13 +41,25 @@ waitFor (Mgr mgr) tid = do
     Nothing -> return Nothing
     Just st -> Just `fmap` takeMVar st
 
--- file: ch24/NiceFork.hs
+-- | Block until all managed threads terminate.
+waitAll :: ThreadManager -> IO ()
 waitAll (Mgr mgr) = modifyMVar mgr elems >>= mapM_ takeMVar
     where elems m = return (M.empty, M.elems m)
 
--- file: ch24/NiceFork.hs
 waitFor2 (Mgr mgr) tid =
   join . modifyMVar mgr $ \m ->
     return $ case M.updateLookupWithKey (\_ _ -> Nothing) tid m of
       (Nothing, _) -> (m, return Nothing)
       (Just st, m') -> (m', Just `fmap` takeMVar st)
+
+test = do
+    m <- newManager
+    tid <- forkManaged m (print "hello")
+    tid2 <- forkManaged m (print "hello")
+    waitAll m
+    print "over"
+
+parRun acts = do
+    m <- newManager
+    mapM_ (forkManaged m) acts
+    waitAll m
