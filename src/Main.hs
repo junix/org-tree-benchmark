20aa7c915db5c95236f89058623d0ef91f854758
wanglijun <juns1984@qq.com>
2016-08-10 11:02:07 +0800

    支持按照组织分表

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,11 @@ import Database.HDBC
 import Database.HDBC.MySQL
 import Data.List (intercalate)
 import Data.Maybe
+import Control.Concurrent
+import Control.Monad (join)
+import qualified Data.Map as M
+
+data ThreadStatus = Running | Finished deriving (Eq, Show)
 
 data Entity = Department Int Int
             | SubCompany Int Int
@@ -175,7 +180,7 @@ join' who dep conn = do
                 run conn sql []
                 commit conn
 
-join who dep = withConn (join' who dep)
+joinIn who dep = withConn (join' who dep)
 
 genChildren (Department org n) cnt = map (Department org . (n*10+)) [0..cnt-1]
 genEmplee   (Department org n) cnt = map (Member org. (n*10+)) [0..cnt-1]
@@ -222,3 +227,65 @@ createOrgs orgs level subCnt = do
     mapM_ (\org -> createOrg' org level subCnt cn) orgs
     disconnect cn
     return ()
+
+
+
+
+
+
+-- file: ch24/NiceFork.hs
+newtype ThreadManager = Mgr (MVar (M.Map ThreadId (MVar ThreadStatus))) deriving (Eq)
+
+-- | Create a new thread manager.
+newManager :: IO ThreadManager
+newManager = Mgr `fmap` newMVar M.empty
+
+-- | Create a new managed thread.
+forkManaged :: ThreadManager -> IO () -> IO ThreadId
+forkManaged (Mgr mgr) body =
+    modifyMVar mgr $ \m -> do
+      state <- newEmptyMVar
+      tid <- forkIO $ do
+        result <- body
+        putMVar state Finished
+      return (M.insert tid state m, tid)
+
+-- | Immediately return the status of a managed thread.
+getStatus :: ThreadManager -> ThreadId -> IO (Maybe ThreadStatus)
+getStatus (Mgr mgr) tid =
+  modifyMVar mgr $ \m ->
+    case M.lookup tid m of
+      Nothing -> return (m, Nothing)
+      Just st -> tryTakeMVar st >>= \mst -> case mst of
+                   Nothing -> return (m, Just Running)
+                   Just sth -> return (M.delete tid m, Just sth)
+
+-- | Block until a specific managed thread terminates.
+waitFor :: ThreadManager -> ThreadId -> IO (Maybe ThreadStatus)
+waitFor (Mgr mgr) tid = do
+  maybeDone <- modifyMVar mgr $ \m ->
+    return $ case M.updateLookupWithKey (\_ _ -> Nothing) tid m of
+      (Nothing, _) -> (m, Nothing)
+      (done, m') -> (m', done)
+  case maybeDone of
+    Nothing -> return Nothing
+    Just st -> Just `fmap` takeMVar st
+
+-- | Block until all managed threads terminate.
+waitAll :: ThreadManager -> IO ()
+waitAll (Mgr mgr) = modifyMVar mgr elems >>= mapM_ takeMVar
+    where elems m = return (M.empty, M.elems m)
+
+-- file: ch24/NiceFork.hs
+waitFor2 (Mgr mgr) tid =
+  join . modifyMVar mgr $ \m ->
+    return $ case M.updateLookupWithKey (\_ _ -> Nothing) tid m of
+      (Nothing, _) -> (m, return Nothing)
+      (Just st, m') -> (m', Just `fmap` takeMVar st)
+
+test = do
+    m <- newManager
+    tid <- forkManaged m (print "hello")
+    tid2 <- forkManaged m (print "hello")
+    waitAll m
+    print "over"
