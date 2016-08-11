96cd261a01aa8be0131e253fc432f67d6120d57f
wanglijun <juns1984@qq.com>
2016-08-11 11:34:21 +0800

    并行创建表

--- src/Main.hs
+++ src/Main.hs
@@ -39,7 +39,7 @@ new' [] conn = return 0
 new' es@(e:_) conn = do
     stmt <- prepare conn (istmt e)
     rs <- mapM (execute stmt . value) es
-    --commit conn
+    commit conn
     return (head rs)
 
 new :: [Entity] -> IO Integer
@@ -76,11 +76,10 @@ join' who dep conn = do
         isql= "INSERT INTO " ++ tab ++ " VALUES (?, ?,?,?,?)"
     run conn isql rec
     if null sql
-        then return () --commit conn
+        then commit conn
         else do
                 run conn sql []
-                --commit conn
-                return ()
+                commit conn
 
 joinIn who dep = withConn (join' who dep)
 
@@ -88,7 +87,7 @@ genChildren (Department org n) cnt = map (Department org . (n*10+)) [0..cnt-1]
 genEmployee (Department org n) cnt = map (Member org. (n*10+)) [0..cnt-1]
 
 itree' level ccnt parent@(Department org pid) conn
-    | pid > 10^level = do { commit conn; return ()}
+    | pid > 10^level = return ()
     | otherwise = do
         let cs = genChildren parent ccnt
         let es = genEmployee parent ccnt
